1ebd46255c8e5e7df0f00d0527764ddfed042888
iaryann <itsmearyantyagi@gmail.com>
2025-02-21 15:55:37 +0530

    JAVA

---
+++ Main.java
@@ -0,0 +1,10 @@
+import Balance.Account;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Account myAccount = new Account(4000.00);
+
+        myAccount.Display_Balance();
+    }
+}
